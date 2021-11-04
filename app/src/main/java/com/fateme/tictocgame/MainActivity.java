0b25de2d1a351162a66afcc7d294b030bc56d4ff
Fateme <feraghi.fateme@gmail.com>
2021-11-04 17:54:13 -0400

    Create OnClickListeners and possible options

--- app/src/main/java/com/fateme/tictocgame/MainActivity.java
+++ app/src/main/java/com/fateme/tictocgame/MainActivity.java
@@ -2,10 +2,14 @@ package com.fateme.tictocgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,13 +31,85 @@ public class MainActivity extends AppCompatActivity {
         game_board = findViewById(R.id.mainBoard);
         txt_turn = findViewById(R.id.turn);
 
-        //resetBoard
+        resetBoard();
+
         txt_turn.setText("Turn: " + turn);
 
         for (int i = 0; i < game_board.getChildCount(); i++) {
+
             TableRow row = (TableRow) game_board.getChildAt(i);
+            for (int j = 0; j < row.getVirtualChildCount(); j++){
+                TextView tv = (TextView) row.getVirtualChildAt(j);
+                tv.setText("");
+                tv.setOnClickListener(Move(i,j,tv));
+            }
         }
 
+
+        Button reset_button = (Button) findViewById(R.id.reset);
+        reset_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent current = getIntent();
+               finish();
+               startActivity(current);
+            }
+        });
+    }
+
+    protected void resetBoard() {
+        turn = 'X';
+
+        for(int i = 0; i < grid_size; i++){
+            for(int j = 0; j < grid_size; j++){
+                my_board[i][j] = ' ';
+            }
+        }
+    }
+
+    protected boolean Cell_Set(int r, int c){
+        // If the condition is true, means the cell is not set to any data(is empty)
+       return !(my_board[r][c] == ' ');
+    }
+
+    View.OnClickListener Move(final int r, final int c, final TextView tv){
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //If the cells are empty
+                if(!Cell_Set(r,c)){
+                    my_board[r][c] = turn;
+
+                    if(turn == 'X'){
+                        tv.setText(R.string.X);
+                        turn = 'O';
+                    } else if(turn == 'O'){
+                        tv.setText(R.string.O);
+                        turn = 'X';
+                    }
+
+                    if(gameStatus() == 0) {
+                        tv.setText("Turn: Player " + turn);
+                    } else if(gameStatus() == -1){
+                        tv.setText("This is a draw match ");
+                        stopMatch();
+                    } else {
+                        txt_turn.setText(turn + "Loses!");
+                        stopMatch();
+                    }
+                } else {
+                    //txt_turn.setText(txt_turn.getText() + " Choose an empty cell");
+                    Toast.makeText(MainActivity.this,"Please, choose an empty cell",Toast.LENGTH_LONG).show();
+                }
+            }
+
+            private int gameStatus() {
+                return 0;
+            }
+
+            private void stopMatch() {
+            }
+        };
     }
 
 }
